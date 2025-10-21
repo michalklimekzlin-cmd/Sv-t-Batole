5da67a303cf67c5d4ffcc331134db83c0d6042e9
michalklimekzlin-cmd <michalklimek.zlin@gmail.com>
2025-10-21 21:14:19 +0200

    Update app.js

--- app.js
+++ app.js
@@ -12,3 +12,23 @@ window.addEventListener("load", () => {
   // přátelský pozdrav z Orbitu
   console.log("%cOrbit: Vivere atque frui 🌱", "color:#7fffd4");
 });
+// === STABILIZACE PÍSMENKOVÉHO PLÁTNA ===
+let memoryText = [];
+
+function drawStableChar(ctx, char, x, y, color) {
+  // Uloží pozici a znak do paměti
+  memoryText.push({ char, x, y, color });
+  // Ořízne paměť (aby se nepřeplnila)
+  if (memoryText.length > 5000) memoryText.shift();
+
+  // Vykreslení nového znaku
+  ctx.fillStyle = color;
+  ctx.fillText(char, x, y);
+}
+
+function redrawAll(ctx) {
+  for (let t of memoryText) {
+    ctx.fillStyle = t.color;
+    ctx.fillText(t.char, t.x, t.y);
+  }
+}
